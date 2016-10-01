b5403f370a95a5de27fecb89c6eb5e85d9f5eb99
Qihong Lu <lvqihong1992@gmail.com>
2016-09-30 20:08:40 -0500

    fix bug: ratio = unique vox / total

--- src/verifyROIs.m
+++ src/verifyROIs.m
@@ -19,24 +19,26 @@ options.nlambda = 100;
 nSubjs = length(SUBJ_NAMES);
 
 %%
-fprintf('subjID\t subjName\t numVox_ROIs\t numVox_cortex\t ROI/cortex \t numVox_ROIs_unique\n')
+fprintf('subjID\t subjName\t numVox_ROIs\t numVox_cortex\t numVox_ROIs_unique \t ROI/cortex \n')
 for s = 1 : nSubjs
     load(fullfile(DIR.DATA, SUBJ_NAMES{s}));
     % record the number of voxels in cortex and predefined ROIs 
     numVox_cortex = length(alldat.grayinds); 
-    numVox_allROIs = 0; 
     idx_ROIs = [];  
     for r = 1 : length(alldat.ROIs) 
         idx_ROIs = horzcat(idx_ROIs, alldat.ROIs(r).ROIind);
-        numVox_allROIs = numVox_allROIs + length(alldat.ROIs(r).ROIind);
     end
-    ratio = numVox_allROIs/numVox_cortex;
-    nonVox_ROIs_unique = length(unique(idx_ROIs));
-    fprintf('%d \t %s \t\t %d \t\t %d \t\t %f \t\t %d \n', ...
-        s, SUBJ_NAMES{s}, numVox_allROIs, numVox_cortex, ratio, nonVox_ROIs_unique);
+    % compute the number of voxels in the ROIs
+    numVox_allROIs = length(idx_ROIs);
+    numVox_ROIs_unique = length(unique(idx_ROIs));
+    ratio = numVox_ROIs_unique/numVox_cortex;
+    
+    fprintf('%d \t %s \t\t %d \t\t %d \t\t %d \t\t\t %f \n', ...
+        s, SUBJ_NAMES{s}, numVox_allROIs, numVox_cortex, numVox_ROIs_unique, ratio);
 end
 
 %% conclusion
-% 1. 40% of the voxels in the cortex are contained in some ROI 
+% 1. About 35% of the voxels in the cortex are contained in some ROI 
 % 2. ROIs are overlapping (about 13%)
-% 3. on average, we have 7000 voxels in the cortex
+% 3. on average, we have 7000 voxels in the cortex, the column:row ratio is
+% comparable to the FPO data
