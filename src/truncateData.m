c6af814885c882554dd5ff25589dffef9392d826
Qihong Lu <lvqihong1992@gmail.com>
2016-11-05 23:59:55 -0500

    subset data matrix w.r.t to ROIs

--- src/truncateData.m
+++ src/truncateData.m
@@ -1,54 +1,60 @@
 clear variables; clc; close all;
 % specify path information
 DIR.PROJECT = '/Users/Qihong/Dropbox/github/motionDecoding_fMRI/';
-DIR.DATA = fullfile(DIR.PROJECT, 'data/');
-DIR.OUT = fullfile(DIR.PROJECT, 'results/'); 
+DIR.DATA = fullfile(DIR.PROJECT, '/data/rawdata/');
+DIR.OUT = fullfile(DIR.PROJECT, 'results/');
 
 % constants
 SUBJ_NAMES = {'ah','br','ds','jf','rl'};
 % specify parameters
 nSubjs = length(SUBJ_NAMES);
-nTR = 16; 
-nClasses = 8; 
+nTR = 16;
+nClasses = 8;
 
-saveTruncatedData = 1
-trunc_name = 'ROIs';
+saveTruncatedData = 1;
+% trunc_name = 'v1';
 
 %% only take a subset of the data
 for s = 1:nSubjs
     load(fullfile(DIR.DATA, SUBJ_NAMES{s}));
     
-    % record subj name, title, coordinates and ROIs 
+    % record subj name, title, coordinates and ROIs
     data.subject_name = SUBJ_NAMES{s};
-    data.title = 'detrened_data'; 
-    data.ROIs = alldat.ROIs; 
+    data.title = 'detrened_data';
+    data.ROIs = alldat.ROIs;
     
-    %% take all ROIs
-    idx_ROIs = [];
-    for i = 1 : length(data.ROIs)
-        idx_ROIs = horzcat(idx_ROIs,data.ROIs(i).ROIind);
-    end
-    idx_ROIs = unique(idx_ROIs);
+    %     %% take all ROIs
+    %     idx_ROIs = [];
+    %     for i = 1
+    % %     for i = 1 : length(data.ROIs)
+    %         idx_ROIs = horzcat(idx_ROIs,data.ROIs(i).ROIind);
+    %     end
+    %     idx_ROIs = unique(idx_ROIs);
+    %
+    %     %% take whole cortex
+    %     idx_cortex = alldat.grayinds;
     
-    %% take whole cortex
-    idx_cortex = alldat.grayinds;
-    
-    %% select the data
-    idx = idx_ROIs;
-    data.coords = alldat.coords(idx,:);
-    % take the functional data, ordered by TR
-    data.detrended = cell(nTR,1);    
-    for t = 1 : nTR
-        % internally, ordered by runs (loop over 10 runs)
-        for r = 1 : length(alldat.detrendedData)
-            data.detrended{t} = vertcat(data.detrended{t}, alldat.detrendedData{r}(:,idx,t));
+    for roi = 1 : length(alldat.ROIs)
+        trunc_name = sprintf('ROI%.2d',roi);
+        %% select the index , get the data
+        data.name = trunc_name; 
+        data.ROIind = alldat.ROIs(roi).ROIind;
+        data.coords = alldat.ROIs(roi).coords';
+        % take the functional data, ordered by TR
+        data.detrended = cell(nTR,1);
+        for t = 1 : nTR
+            % internally, ordered by runs (loop over 10 runs)
+            for run = 1 : length(alldat.detrendedData)
+                data.detrended{t} = vertcat(data.detrended{t}, ...
+                    alldat.detrendedData{run}(:,data.ROIind,t));
+            end
         end
-    end
-    
-    % save data
-    if saveTruncatedData
-        fprintf('Truncated data for <%s> is saved to <%s>\n', ...
-            data.subject_name, DIR.DATA);
-        save(strcat(DIR.DATA,data.subject_name,'_',trunc_name),'data');
+        
+        % save data
+        if saveTruncatedData
+            fprintf('Truncated data for <%s> is saved to <%s>\n', ...
+                data.subject_name, DIR.DATA);
+            save(strcat(DIR.DATA,data.subject_name,'_',trunc_name),'data');
+        end
     end
 end
