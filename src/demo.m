3de2e4a5ddcc24f8131ac71c20f861ac6db4d938
Qihong Lu <lvqihong1992@gmail.com>
2017-02-27 15:40:51 -0600

    simplify

--- src/demo.m
+++ src/demo.m
@@ -1,11 +1,10 @@
-clear variables; 
-analysis_names = {'wb', 'ROI01'};
+clear variables;
+analysis_names = {'ROI01', 'wb'};
 
 % specify path information
 DIR.ROOT = '..';
 DIR.DATA = fullfile(DIR.ROOT, 'demo_data/');
 DIR.OUT = fullfile(DIR.ROOT, 'results/');
-model_name = 'logistic lasso with min dev lambda';
 decodingObjective = '2d';
 
 % parameters
@@ -20,28 +19,25 @@ NRUNS = 10;
 % set up target vectors
 [y, ROW_MASK, unit_mask] = getMaskAndRange(decodingObjective, NRUNS);
 
-for roi = 1 : length(analysis_names)
+for roi = 1
     % specify parameters
     nSubjs = length(SUBJ_NAMES);
     FILENAMES = strcat(SUBJ_NAMES, {strcat('_', analysis_names{roi}, '.mat')});
     load(fullfile(DIR.DATA, FILENAMES{1}));
     
     % loop over TR ("time")
-    for t = 5 
+    for t = 5
         % select horizontal-depth data
         X = data.detrended{t}(~ROW_MASK,:);
         
-        % decode for all CVB
-        for c = 1 
-            % set up the test index
-            idx_testset = false(size(X,1),1);
-            idx_testset(1 : (TEST_TRIALS * sum(~unit_mask))) = true;
-            
-            % fit logistic lasso
-            % YOUR CODE HERE! 
-            
+        % set up the test index
+        idx_testset = false(size(X,1),1);
+        idx_testset(1 : (TEST_TRIALS * sum(~unit_mask))) = true;
+        
+        % fit logistic lasso
+        % YOUR CODE HERE!
+        %% hold out the test set
 
-                        
-        end
+        
     end
 end
