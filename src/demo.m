6428534da0ad9b784b5350a9886f5f2b5f126d4e
Qihong Lu <lvqihong1992@gmail.com>
2017-02-26 22:46:24 -0600

    simplify the demo code

--- src/demo.m
+++ src/demo.m
@@ -10,36 +10,24 @@ decodingObjective = '2d';
 
 % parameters
 NCVB = 5; % 5-folds cross validation
-NCVB_internal = 4;
 TEST_TRIALS = 4;
 options.nlambda = 100;
 options.alpha = 1;
-
-
-% CONSTANTS (should not be changed)
 SUBJ_NAMES = {'ah'};
 NTR = 16;
 NRUNS = 10;
-CVB_UNIT = NRUNS/NCVB;
-NBLOCK_TRAIN = CVB_UNIT * (NCVB-1);
 
 % set up target vectors
 [y, ROW_MASK, unit_mask] = getMaskAndRange(decodingObjective, NRUNS);
 
 for roi = 1 : length(analysis_names)
-    fprintf('%s \n', analysis_names{roi})
     % specify parameters
     nSubjs = length(SUBJ_NAMES);
     FILENAMES = strcat(SUBJ_NAMES, {strcat('_', analysis_names{roi}, '.mat')});
     load(fullfile(DIR.DATA, FILENAMES{1}));
     
-    % preallocate
-    RESULTS.accuracy = nan(NTR,NCVB);
-    RESULTS.lambda_min = nan(NTR,NCVB);
-    RESULTS.coef = nan(size(data.coords,1)+1,NCVB, NTR);
     % loop over TR ("time")
     for t = 5 
-        fprintf('%d ', t);
         % select horizontal-depth data
         X = data.detrended{t}(~ROW_MASK,:);
         
@@ -51,13 +39,7 @@ for roi = 1 : length(analysis_names)
             
             % fit logistic lasso
             % YOUR CODE HERE! 
-            runLassoGlm(X, y, idx_testset, options, NCVB, 'lassoglm')
-            
-            % record the results
-            RESULTS.accuracy(t,c) = results.lasso_accuracy_lambda_min;
-            RESULTS.lambda_min(t,c) = results.lasso_lambda_min;
-            RESULTS.coef(:,c,t) = results.lasso_coef_lambda_min;
+                        
         end
     end
-    fprintf('\n');
 end
